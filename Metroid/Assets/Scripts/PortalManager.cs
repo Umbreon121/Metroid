cfa649450c1ec273e99ae2525054672563000445
Umbreon121 <148890817+Umbreon121@users.noreply.github.com>
2023-11-13 16:22:42 -0800

    Final Push

--- Metroid/Assets/Scripts/PortalManager.cs
+++ Metroid/Assets/Scripts/PortalManager.cs
@@ -9,17 +9,5 @@ using UnityEngine;
 public class Portal1 : MonoBehaviour
 {
     //game object to tell where the spawn point is
-    public GameObject spawnPoint1;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    public GameObject spawnPoint;
 }
